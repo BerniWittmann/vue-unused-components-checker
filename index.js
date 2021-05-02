ae88aea78f7705b9e8e6a2d3cb79aa0c4d69ee45
Carlos Carvalho <carlos.lm.carvalho@gmail.com>
2021-05-02 17:23:08 -0300

    fix: :arrow_up: Upgrade Command and fixes optional src argument
    Running without src seemed to halt execution. Upgraded Command to make it easier to consume args following current documentation

--- index.js
+++ index.js
@@ -5,11 +5,16 @@ var path = require('path');
 var checker = require('./checker');
 
 program
-  .arguments('<src>')
+  .version('1.2.0')
+  .arguments('[src]')
+  .description('Lists unused vue components in project', {
+    src: 'Root folder to inspect, if not provided, will default to current folder'
+  })
   .option('-o, --openfiles <number>', 'Number of Max Open files')
   .option('-i, --ignore <glob>', 'glob pattern or Array of glob patterns to ignore', ['**/node_modules/**', '**/.nuxt/**', '**/dist/**', '**/coverage/**'])
-  .action(function (src) {
-    var completePath = src ? path.join(process.cwd(), src) : process.cwd();
-    checker(completePath, program.openfiles, program.ignore);
+  .action(function (src, options, command) {
+    const completePath = src ? path.join(process.cwd(), src) : process.cwd();
+    console.log("Checking unused components from root folder: ", completePath)
+    checker(completePath, options.openfiles, options.ignore);
   })
   .parse(process.argv);
